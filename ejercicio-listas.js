8c7696d3312771b7fb3e33627abd2fe6bfb75344
Jehiel Linarez <122569500+JehielL@users.noreply.github.com>
2023-01-15 21:43:22 +0100

    Add files via upload

---
+++ ejercicio-listas.js
@@ -0,0 +1,14 @@
+new Array = {
+    nombre: jehiel,
+    edad : 31,
+    eresProgramador: true,
+    new:Date("11 de febrero de 1992"),
+}
+
+const libroFavorito ={
+    tituto: "Harry potter",
+    autor: "JK ROWLING",
+    fecha: new Date("1997"),
+    url: "https://es.wikipedia.org/wiki/Harry_Potter"
+}
+
