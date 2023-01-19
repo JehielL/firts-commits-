2b41f44f01706b0c09590d2014d3d5fd312ee04e
Jehiel Linarez <122569500+JehielL@users.noreply.github.com>
2023-01-19 17:06:54 +0100

    Add files via upload

---
+++ lineas-de-texto.js
@@ -0,0 +1,29 @@
+let nombre = "Jehiel"
+let apellido = "Linarez"
+let estudiante = nombre.concat(" ").concat(apellido) 
+
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+
+let estudianteLenght = estudiante.length
+
+console.log(estudianteLenght)
+
+let inicialNombre = nombre.substring(0, 1)
+
+console.log(inicialNombre)
+
+let finalApellido = apellido.substring(apellido.length -1, apellido.length)
+
+console.log(finalApellido)
+
+let estudianteSinEspacios = estudiante.replace(/ /g,"")
+
+console.log(estudianteSinEspacios)
+
+let nombreEnEstudiante = estudiante.includes("Jehiel")
+
+console.log(nombreEnEstudiante)
