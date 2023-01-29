964b5f4764c90dd94bbe459f8ae4b2da0ff5c804
Jehiel Linarez <122569500+JehielL@users.noreply.github.com>
2023-01-29 11:01:46 +0100

    Add files via upload

---
+++ numeros-red.js
@@ -0,0 +1,14 @@
+let altura_cm = 173
+let altura_m = 1.73
+let peso_Kg = 85.50
+
+
+let altura_redon = Math.ceil(altura_m)
+console.log(altura_redon)
+
+let pesoKg_redon = Math.floor(peso_Kg)
+console.log(pesoKg_redon)
+
+let maxValue = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+
+console.log(maxValue)
